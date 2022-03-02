f629f5696fa118274563d552409f80227ab6c32a
91nakashima <91nakashima@gmail.com>
2022-03-02 13:43:50 +0900

    分岐を微修正

--- src/easyGetData.ts
+++ src/easyGetData.ts
@@ -38,53 +38,10 @@ export async function easyGetData<T> (
     }
   }
 
-  if (collectionArray.length === 1 || collectionArray.length % 2 === 1) {
-    /**
-     * document
-     * https://firebase.google.com/docs/firestore/query-data/queries?hl=ja#simple_queries
-     */
-    if (option.where) {
-      if (!Array.isArray(option.where)) return new Error()
-
-      option.where.map((w: WhereOption) => {
-        if (!isUseType(reference)) return w
-        reference = reference.where(w[0], w[1], w[2])
-        return w
-      })
-    }
-
-    /**
-     * document
-     * https://firebase.google.com/docs/firestore/query-data/order-limit-data?hl=ja#order_and_limit_data
-     */
-    if (option.orderBy) {
-      if (!isUseType(reference)) return new Error()
-      reference = reference.orderBy(option.orderBy)
-    }
-
-    /**
-     * document
-     * https://firebase.google.com/docs/firestore/query-data/order-limit-data?hl=ja#order_and_limit_data
-     */
-    if (option.limit) {
-      if (!isUseType(reference)) return new Error()
-      reference = reference.limit(option.limit)
-    }
-
-    if (!isUseType(reference)) return new Error()
-    const res = await reference.get()
-
-    /**
-     * document data in Array
-     */
-    const arr: Array<T> = []
-    res.forEach(el => {
-      if (!el.exists) return
-      arr.push(el.data() as T)
-    })
-
-    return arr
-  } else if (collectionArray.length % 2 === 0) {
+  /**
+   * DocumentReferenceの場合
+   */
+  if (reference instanceof DocumentReference) {
     return new Promise((resolve, rejects) => {
       if (!(reference instanceof DocumentReference)) return rejects()
       reference
@@ -96,5 +53,50 @@ export async function easyGetData<T> (
         .catch(() => rejects())
     })
   }
-  return new Error()
+
+  /**
+   * document
+   * https://firebase.google.com/docs/firestore/query-data/queries?hl=ja#simple_queries
+   */
+  if (option.where) {
+    if (!Array.isArray(option.where)) return new Error()
+
+    option.where.map((w: WhereOption) => {
+      if (!isUseType(reference)) return w
+      reference = reference.where(w[0], w[1], w[2])
+      return w
+    })
+  }
+
+  /**
+   * document
+   * https://firebase.google.com/docs/firestore/query-data/order-limit-data?hl=ja#order_and_limit_data
+   */
+  if (option.orderBy) {
+    if (!isUseType(reference)) return new Error()
+    reference = reference.orderBy(option.orderBy)
+  }
+
+  /**
+   * document
+   * https://firebase.google.com/docs/firestore/query-data/order-limit-data?hl=ja#order_and_limit_data
+   */
+  if (option.limit) {
+    if (!isUseType(reference)) return new Error()
+    reference = reference.limit(option.limit)
+  }
+
+  if (!isUseType(reference)) return new Error()
+  const res = await reference.get()
+
+  /**
+   * document data in Array
+   */
+  const arr: Array<T> = []
+  res.forEach(el => {
+    if (!el.exists) return
+    arr.push(el.data() as T)
+  })
+
+  return arr
 }
